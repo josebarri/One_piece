f660315fa2b5cb6c51e5bad34ef3f16e908f1fd8
josebarri <josealfredobarriossalgado@gmail.com>
2023-05-01 19:17:32 -0500

    update

--- src/views/FrutasView.vue
+++ src/views/FrutasView.vue
@@ -1,15 +1,14 @@
 <script setup>
-import FooterComp from '../components/Global/FooterComp.vue';
-import NavComp from '../components/Global/NavComp.vue';
-import FruComp from '../components/fruits/FruComp.vue';
-import FruInfoCompVue from '../components/fruits/FruInfoComp.vue';
-import CategoComp from '../components/fruits/CategoComp.vue';
-
+import FooterComp from '../components/Global/FooterComp.vue'
+import NavComp from '../components/Global/NavComp.vue'
+import FruComp from '../components/fruits/FruComp.vue'
+import FruinfoComp from '../components/fruits/FruinfoComp.vue'
+import CategoComp from '../components/fruits/CategoComp.vue'
 </script>
 <template>
-    <NavComp />
-    <FruInfoCompVue />
-    <CategoComp />
-    <FruComp />
-    <FooterComp />
+  <NavComp />
+  <FruinfoComp />
+  <CategoComp />
+  <FruComp />
+  <FooterComp />
 </template>
