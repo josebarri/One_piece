da224857ea40a9540a884d2f300afdc8160e2934
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-03-03 20:23:27 -0500

    Se realizo commit por obligacion

---
+++ src/views/PruebaView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import FooterComp from '../components/Global/FooterComp.vue'
+import NavComp from '../components/Global/NavComp.vue'
+import TiposComp from '../components/fruits/TiposComp.vue'
+</script>
+<template>
+  <NavComp />
+  <TiposComp/>
+  <FooterComp />
+</template>
